f34ebb091a3d6fd97c5da45f659a87775f33027f
Sonia006 <ssgalelm@etsinf.upv.es>
2025-12-23 18:54:03 +0100

    Comentarios BSNode.h

--- DIA5/BSNode.h
+++ DIA5/BSNode.h
@@ -4,18 +4,19 @@
 #include <ostream>
 
 template <typename T> 
+// Clase del nodo con inervalos.
 class BSNode {
     public:
-    BSNode<T>* left;
-	BSNode<T>* right;
-	T inicio;
-	T fin;
-	T maxfin;
+    BSNode<T>* left;			// Puntero a hijo izquierdo.
+	BSNode<T>* right;			// Puntero a hijo derecho.	
+	T inicio;					// Inicio del rango/intervalo.
+	T fin;						// Fin del rango/intervalo.
+	T maxfin;					// Valor maximo de la rama.
 	
-	BSNode(T inicio, T fin);
+	BSNode(T inicio, T fin);	// Constructor.
 
 };
-
+//Constructor, inicia el nodo con los punteros apuntando a nullptr y las variables de los rangos con los valores que se le mete como argumento.
 template <typename T>
 BSNode<T>::BSNode(T inicio, T fin){
 	this->inicio = inicio;
